644cb6e39b8e2d93502e3352f38feba5e60c170b
Alex Cleveland <alexcleveland5@gmail.com>
2026-02-28 09:35:54 -0700

    Store API Key In env

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import requests
-api_key = "30be9d6f81ee076d9023bf7273f7e725"
+import os
+from twilio.rest import Client
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+    from_="+15017122661",
+    to="+15558675310",
+)
+
+print(message.body)
+
+api_key = os.environ.get("API_KEY")
 lit_lat = "39.604652"
 lit_lon = "-105.123779"
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
